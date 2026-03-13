b1aaf8dfea54a34c26d9d62fc2b900d80a0165f8
huplert <140969061+huplert@users.noreply.github.com>
2026-03-13 01:16:03 +0000

    git add . git commit -m "fix maps global" git push

--- js/maps.js
+++ js/maps.js
@@ -1,27 +1,32 @@
 function makeTown(){
 
-const w=32
-const h=32
+const w = 32
+const h = 32
 
-const bg=[]
-const collision=[]
+const bg = make2D(w,h,Tile.GRASS)
+const collision = make2D(w,h,0)
 
 for(let y=0;y<h;y++){
-
-bg[y]=[]
-collision[y]=[]
-
 for(let x=0;x<w;x++){
 
-bg[y][x]=Tile.GRASS
-collision[y][x]=0
-
+if(x===0||y===0||x===w-1||y===h-1){
+bg[y][x]=Tile.TREE
+collision[y][x]=1
 }
 
+}
+}
+
+for(let x=4;x<28;x++){
+bg[16][x]=Tile.PATH
+}
+
+for(let y=8;y<24;y++){
+bg[y][16]=Tile.PATH
 }
 
 return{
-id:"town",
+id:"starter_town",
 name:"Starter Town",
 width:w,
 height:h,
@@ -33,4 +38,8 @@ warps:[],
 encounters:null
 }
 
+}
+
+window.maps = {
+starter_town: makeTown()
 }
