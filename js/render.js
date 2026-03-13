3e095a4bca1baf42e0fdbd5d3afb3a59cb2b63b0
huplert <140969061+huplert@users.noreply.github.com>
2026-03-12 21:11:02 -0400

    Refactor rendering functions for better clarity
    Refactor drawTile and drawMap functions for improved readability and consistency. Update case statements to use Tile constants.

--- js/render.js
+++ js/render.js
@@ -1,33 +1,61 @@
-function drawTile(tile,x,y){
+function drawTile(tile, x, y) {
 
 switch(tile){
 
-case 0:
-ctx.fillStyle=COLORS.c3
+case Tile.GRASS:
+ctx.fillStyle = COLORS.c3
 break
 
-case 1:
-ctx.fillStyle=COLORS.c2
+case Tile.PATH:
+ctx.fillStyle = COLORS.c2
 break
 
-case 2:
-ctx.fillStyle=COLORS.c1
+case Tile.TREE:
+ctx.fillStyle = COLORS.c1
 break
+
+case Tile.TALL_GRASS:
+ctx.fillStyle = COLORS.c2
+break
+
+default:
+ctx.fillStyle = COLORS.c3
 
 }
 
-ctx.fillRect(x,y,TILE,TILE)
+ctx.fillRect(x, y, TILE, TILE)
 
 }
 
 function drawMap(map){
 
-for(let y=0;y<map.height;y++){
-for(let x=0;x<map.width;x++){
+for(let y=0; y<map.height; y++){
+for(let x=0; x<map.width; x++){
 
-drawTile(map.bg[y][x],x*TILE,y*TILE)
+drawTile(map.bg[y][x], x*TILE, y*TILE)
 
 }
 }
+
+}
+
+function drawPlayer(player){
+
+ctx.fillStyle = COLORS.c0
+ctx.fillRect(player.x, player.y, PLAYER_SIZE, PLAYER_SIZE)
+
+}
+
+function render(state){
+
+ctx.fillStyle = COLORS.c3
+ctx.fillRect(0,0,SCREEN_W,SCREEN_H)
+
+drawMap(state.map)
+
+drawPlayer(state.player)
+
+ctx.font = "bold 8px monospace"
+drawText(state.map.name,4,4)
 
 }
